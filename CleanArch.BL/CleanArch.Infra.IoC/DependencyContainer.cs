2d4b03832216dff83bde35b68539ae88a34fd645
Agaba Kabunga <agaba_k@hotmail.com>
2020-03-21 02:09:29 -0500

    Add  dependency for all the project under Ioc project

--- CleanArch.BL/CleanArch.Infra.IoC/DependencyContainer.cs
+++ CleanArch.BL/CleanArch.Infra.IoC/DependencyContainer.cs
@@ -1,7 +1,13 @@
 ﻿using CleanArch.Application.Interface;
 using CleanArch.Application.Services;
+using CleanArch.Domain.CommandHandlers;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.Core.Bus;
 using CleanArch.Domain.Interfaces;
+using CleanArch.Info.Data.Context;
 using CleanArch.Info.Data.Repositories;
+using CleanArch.Infra.Bus;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -11,14 +17,23 @@ namespace CleanArch.Infra.IoC
 {
     public class DependencyContainer
     {
+        
         // static because need to available all the time
         public static void RegisterService(IServiceCollection services)
         {
+            // Domain InMemory Mediator
+            services.AddScoped<IMediatorHandler, InMemoryBus>();
+
+            // Domain Handler
+            services.AddScoped<IRequestHandler<CreateCourseCommand, bool>, CourseCommandHandler>();
+
             //Application Layer 
             services.AddScoped<ICourseService, CourseService>();
 
             // Data Layer 
             services.AddScoped<ICourseRepository, CourseRepository>();
+
+            services.AddScoped<UniversityDbContext>();
 
 
         }
